712ea2b53a31de5ed58ef20e31787261bd708d88
Anthony DiBernardo <adibern2@charlotte.edu>
2026-03-06 16:01:52 -0500

    added extrordinaroly basic auton

--- xRobot/src/autonomous.py
+++ xRobot/src/autonomous.py
@@ -2,4 +2,12 @@ from config import * # lsp-only
 def autonomous():
     brain.screen.clear_screen()
     brain.screen.print("autonomous code")
-    # place autonomous code here
+    intake.spin(FORWARD)
+    matchLoad.open()
+    drive_state = DriveState()
+    driveToPoint(-48, 48, 90, drive_state)
+    driveToPoint(-72, 48, 90, drive_state)
+    heightMech.open()
+    driveToPoint(-24, 48, 90, drive_state)
+    outFlex.spin(FORWARD)
+
